a9b1d5c9bf542d739dfa2a58b3c059fc97b17b73
FAANN <103813999+FANRUIHAO@users.noreply.github.com>
2025-01-17 11:14:16 +0800

    uer

---
+++ src/main/java/domain/User.java
@@ -0,0 +1,40 @@
+package domain;
+
+public class User {
+    private Integer id;
+    private String user_name;
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", user_name='" + user_name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
