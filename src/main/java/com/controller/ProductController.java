c8a2b9a43de2afc27e025d5fb192cbed25587cae
FAANN <103813999+FANRUIHAO@users.noreply.github.com>
2025-03-17 15:15:45 +0800

    bug用户修改后当前用户切换至修改的用户

--- src/main/java/com/controller/ProductController.java
+++ src/main/java/com/controller/ProductController.java
@@ -33,6 +33,7 @@ public class ProductController {
         model.addAttribute("products", pageInfo.getList());
         return "product/list";
     }
+
     String uploadDir = new File("target/classes/static/images/").getAbsolutePath();
     File uploadDirFile = new File(uploadDir);
     @RequestMapping("/save")
